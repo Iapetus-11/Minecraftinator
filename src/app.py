2c91b9cb9333868b35c721bf70ccab9abc5471b2
Iapetus-11 <olimdahacker@gmail.com>
2020-11-10 14:00:49 -0500

    start base app

--- src/app.py
+++ src/app.py
@@ -1,8 +1,21 @@
-import aiohttp
+from aiohttp import web
+import aiohttp_jinja2
 import asyncio
+import jinja2
 
-router = aiohttp.web.RouteTableDef()
+router = web.RouteTableDef()
 
 @router.get('/')
-async def index(req: aiohttp.web.Request):
-    
+@aiohttp_jinja2.template('index.html')
+async def index(req: web.Request):
+    pass
+
+async def init():
+    app = web.Application()  # app instance
+    app.add_routes(router)  # add routes
+
+    aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader('./pages'))  # load templates/pages
+
+    return app
+
+web.run_app(init())
